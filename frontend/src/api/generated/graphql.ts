deda8ced385d714c18857d3e4cf3d48695ea03f3
Colton Travers <contactcolt@gmail.com>
2021-06-06 17:23:22 -0400

    New generated graphql.

--- frontend/src/api/generated/graphql.ts
+++ frontend/src/api/generated/graphql.ts
@@ -10,12 +10,17 @@ export type Scalars = {
   Boolean: boolean;
   Int: number;
   Float: number;
+  DateTime: any;
 };
 
 export type Query = {
   __typename?: 'Query';
   classroom?: Maybe<Classroom>;
   classrooms: Array<Classroom>;
+  user?: Maybe<User>;
+  users: Array<User>;
+  event?: Maybe<Event>;
+  events: Array<Event>;
 };
 
 
@@ -32,14 +37,54 @@ export type QueryClassroomsArgs = {
   after?: Maybe<ClassroomWhereUniqueInput>;
 };
 
+
+export type QueryUserArgs = {
+  where: UserWhereUniqueInput;
+};
+
+
+export type QueryUsersArgs = {
+  where?: Maybe<UserWhereInput>;
+  first?: Maybe<Scalars['Int']>;
+  last?: Maybe<Scalars['Int']>;
+  before?: Maybe<UserWhereUniqueInput>;
+  after?: Maybe<UserWhereUniqueInput>;
+};
+
+
+export type QueryEventArgs = {
+  where: EventWhereUniqueInput;
+};
+
+
+export type QueryEventsArgs = {
+  where?: Maybe<EventWhereInput>;
+  first?: Maybe<Scalars['Int']>;
+  last?: Maybe<Scalars['Int']>;
+  before?: Maybe<EventWhereUniqueInput>;
+  after?: Maybe<EventWhereUniqueInput>;
+};
+
 export type Mutation = {
   __typename?: 'Mutation';
   createOneclassroom: Classroom;
+  createOneuser: User;
+  createOneevent: Event;
 };
 
 
 export type MutationCreateOneclassroomArgs = {
   data: ClassroomCreateInput;
+};
+
+
+export type MutationCreateOneuserArgs = {
+  data: UserCreateInput;
+};
+
+
+export type MutationCreateOneeventArgs = {
+  data: EventCreateInput;
 };
 
 export type Classroom = {
@@ -49,6 +94,19 @@ export type Classroom = {
   teacher: Scalars['String'];
   time: Scalars['String'];
   grade: Scalars['Int'];
+};
+
+export type User = {
+  __typename?: 'user';
+  id: Scalars['Int'];
+  email: Scalars['String'];
+};
+
+export type Event = {
+  __typename?: 'event';
+  id: Scalars['Int'];
+  name: Scalars['String'];
+  user_id?: Maybe<Scalars['Int']>;
 };
 
 export type ClassroomWhereUniqueInput = {
@@ -72,7 +130,57 @@ export type ClassroomCreateInput = {
   teacher: Scalars['String'];
   grade: Scalars['Int'];
   time: Scalars['String'];
-  grades?: Maybe<GradeCreateManyWithoutClassroomInput>;
+  grades?: Maybe<GradeCreateNestedManyWithoutClassroomInput>;
+};
+
+export type UserWhereUniqueInput = {
+  id?: Maybe<Scalars['Int']>;
+  email?: Maybe<Scalars['String']>;
+};
+
+export type UserWhereInput = {
+  AND?: Maybe<Array<UserWhereInput>>;
+  OR?: Maybe<Array<UserWhereInput>>;
+  NOT?: Maybe<Array<UserWhereInput>>;
+  id?: Maybe<IntFilter>;
+  givenName?: Maybe<StringFilter>;
+  familyName?: Maybe<StringFilter>;
+  email?: Maybe<StringFilter>;
+  password?: Maybe<StringFilter>;
+  createdAt?: Maybe<DateTimeFilter>;
+  updatedAt?: Maybe<DateTimeFilter>;
+  apiToken?: Maybe<StringFilter>;
+  event?: Maybe<EventListRelationFilter>;
+};
+
+export type UserCreateInput = {
+  givenName: Scalars['String'];
+  familyName: Scalars['String'];
+  email: Scalars['String'];
+  password: Scalars['String'];
+  createdAt?: Maybe<Scalars['DateTime']>;
+  updatedAt?: Maybe<Scalars['DateTime']>;
+  apiToken: Scalars['String'];
+  event?: Maybe<EventCreateNestedManyWithoutUserInput>;
+};
+
+export type EventWhereUniqueInput = {
+  id?: Maybe<Scalars['Int']>;
+};
+
+export type EventWhereInput = {
+  AND?: Maybe<Array<EventWhereInput>>;
+  OR?: Maybe<Array<EventWhereInput>>;
+  NOT?: Maybe<Array<EventWhereInput>>;
+  id?: Maybe<IntFilter>;
+  name?: Maybe<StringFilter>;
+  user_id?: Maybe<IntNullableFilter>;
+  user?: Maybe<UserWhereInput>;
+};
+
+export type EventCreateInput = {
+  name: Scalars['String'];
+  user?: Maybe<UserCreateNestedOneWithoutEventInput>;
 };
 
 export type IntFilter = {
@@ -107,9 +215,53 @@ export type GradeListRelationFilter = {
   none?: Maybe<GradeWhereInput>;
 };
 
-export type GradeCreateManyWithoutClassroomInput = {
+export type GradeCreateNestedManyWithoutClassroomInput = {
   create?: Maybe<Array<GradeCreateWithoutClassroomInput>>;
+  connectOrCreate?: Maybe<Array<GradeCreateOrConnectWithoutClassroomInput>>;
+  createMany?: Maybe<GradeCreateManyClassroomInputEnvelope>;
   connect?: Maybe<Array<GradeWhereUniqueInput>>;
+};
+
+export type DateTimeFilter = {
+  equals?: Maybe<Scalars['DateTime']>;
+  in?: Maybe<Array<Scalars['DateTime']>>;
+  notIn?: Maybe<Array<Scalars['DateTime']>>;
+  lt?: Maybe<Scalars['DateTime']>;
+  lte?: Maybe<Scalars['DateTime']>;
+  gt?: Maybe<Scalars['DateTime']>;
+  gte?: Maybe<Scalars['DateTime']>;
+  not?: Maybe<NestedDateTimeFilter>;
+};
+
+export type EventListRelationFilter = {
+  every?: Maybe<EventWhereInput>;
+  some?: Maybe<EventWhereInput>;
+  none?: Maybe<EventWhereInput>;
+};
+
+
+export type EventCreateNestedManyWithoutUserInput = {
+  create?: Maybe<Array<EventCreateWithoutUserInput>>;
+  connectOrCreate?: Maybe<Array<EventCreateOrConnectWithoutUserInput>>;
+  createMany?: Maybe<EventCreateManyUserInputEnvelope>;
+  connect?: Maybe<Array<EventWhereUniqueInput>>;
+};
+
+export type IntNullableFilter = {
+  equals?: Maybe<Scalars['Int']>;
+  in?: Maybe<Array<Scalars['Int']>>;
+  notIn?: Maybe<Array<Scalars['Int']>>;
+  lt?: Maybe<Scalars['Int']>;
+  lte?: Maybe<Scalars['Int']>;
+  gt?: Maybe<Scalars['Int']>;
+  gte?: Maybe<Scalars['Int']>;
+  not?: Maybe<NestedIntNullableFilter>;
+};
+
+export type UserCreateNestedOneWithoutEventInput = {
+  create?: Maybe<UserCreateWithoutEventInput>;
+  connectOrCreate?: Maybe<UserCreateOrConnectWithoutEventInput>;
+  connect?: Maybe<UserWhereUniqueInput>;
 };
 
 export type NestedIntFilter = {
@@ -158,19 +310,43 @@ export type GradeCreateWithoutClassroomInput = {
   grade: Scalars['Int'];
 };
 
+export type GradeCreateOrConnectWithoutClassroomInput = {
+  where: GradeWhereUniqueInput;
+  create: GradeCreateWithoutClassroomInput;
+};
+
+export type GradeCreateManyClassroomInputEnvelope = {
+  data?: Maybe<Array<GradeCreateManyClassroomInput>>;
+  skipDuplicates?: Maybe<Scalars['Boolean']>;
+};
+
 export type GradeWhereUniqueInput = {
   id?: Maybe<Scalars['Int']>;
 };
 
-export type IntNullableFilter = {
-  equals?: Maybe<Scalars['Int']>;
-  in?: Maybe<Array<Scalars['Int']>>;
-  notIn?: Maybe<Array<Scalars['Int']>>;
-  lt?: Maybe<Scalars['Int']>;
-  lte?: Maybe<Scalars['Int']>;
-  gt?: Maybe<Scalars['Int']>;
-  gte?: Maybe<Scalars['Int']>;
-  not?: Maybe<NestedIntNullableFilter>;
+export type NestedDateTimeFilter = {
+  equals?: Maybe<Scalars['DateTime']>;
+  in?: Maybe<Array<Scalars['DateTime']>>;
+  notIn?: Maybe<Array<Scalars['DateTime']>>;
+  lt?: Maybe<Scalars['DateTime']>;
+  lte?: Maybe<Scalars['DateTime']>;
+  gt?: Maybe<Scalars['DateTime']>;
+  gte?: Maybe<Scalars['DateTime']>;
+  not?: Maybe<NestedDateTimeFilter>;
+};
+
+export type EventCreateWithoutUserInput = {
+  name: Scalars['String'];
+};
+
+export type EventCreateOrConnectWithoutUserInput = {
+  where: EventWhereUniqueInput;
+  create: EventCreateWithoutUserInput;
+};
+
+export type EventCreateManyUserInputEnvelope = {
+  data?: Maybe<Array<EventCreateManyUserInput>>;
+  skipDuplicates?: Maybe<Scalars['Boolean']>;
 };
 
 export type NestedIntNullableFilter = {
@@ -182,6 +358,32 @@ export type NestedIntNullableFilter = {
   gt?: Maybe<Scalars['Int']>;
   gte?: Maybe<Scalars['Int']>;
   not?: Maybe<NestedIntNullableFilter>;
+};
+
+export type UserCreateWithoutEventInput = {
+  givenName: Scalars['String'];
+  familyName: Scalars['String'];
+  email: Scalars['String'];
+  password: Scalars['String'];
+  createdAt?: Maybe<Scalars['DateTime']>;
+  updatedAt?: Maybe<Scalars['DateTime']>;
+  apiToken: Scalars['String'];
+};
+
+export type UserCreateOrConnectWithoutEventInput = {
+  where: UserWhereUniqueInput;
+  create: UserCreateWithoutEventInput;
+};
+
+export type GradeCreateManyClassroomInput = {
+  id?: Maybe<Scalars['Int']>;
+  name: Scalars['String'];
+  grade: Scalars['Int'];
+};
+
+export type EventCreateManyUserInput = {
+  id?: Maybe<Scalars['Int']>;
+  name: Scalars['String'];
 };
 
 export type ClassroomsQueryVariables = Exact<{
